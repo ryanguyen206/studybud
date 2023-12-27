2cb9cede7353e69c0814c9b605311533db8afadc
ryanguyen206 <ryanninja595@gmail.com>
2023-12-27 15:20:18 -0800

    add logout/login functionlity

--- base/views.py
+++ base/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-# rooms = [
-#     {'id': 1, 'name': 'Lets learn python'},
-#     {'id': 2, 'name': 'Swag!'},
-#     {'id': 3, 'name': 'Frontend developers'},
-# ]
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Invalid credentials')
+        
+        user = authenticate(request, username = username, password = password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+              messages.error(request, 'User or password does not exist')
+        
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, 'base/home.html', {'rooms': rooms})
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains = q)| Q(name__icontains = q) | Q(descripton__icontains = q))
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    return render(request, 'base/home.html', {'rooms': rooms, 'topics': topics, 'room_count': room_count})
 
 def room(request, id):
     room = Room.objects.get(id=id)
