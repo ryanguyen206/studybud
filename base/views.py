2e29c73392ab35375559b1727384ec08bdc68ef3
ryanguyen206 <ryanninja595@gmail.com>
2023-12-26 16:21:47 -0800

    boilerplate + add routes

---
+++ base/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Swag!'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
+def home(request):
+    return render(request, 'base/home.html', {'rooms': rooms})
+
+def room(request, id):
+    room = None
+    for i in rooms:
+        if i['id'] == int(id):
+            room = i
+    context = {'room' : room}
+    return render(request, 'base/room.html', context)
+
