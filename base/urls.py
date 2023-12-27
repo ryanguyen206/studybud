09015795d81b59a4325b30ed2f7a43d9f6765b72
ryanguyen206 <ryanninja595@gmail.com>
2023-12-26 18:16:21 -0800

    create CRUD operations

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home,  name='home'),
     path('room/<str:id>/', views.room, name="room"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:id>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:id>/', views.deleteRoom, name="delete-room")
 ]
